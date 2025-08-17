d1d29ce4cfcf3f3c04cf1bb32f7d68c4acc5a804
Arpit Maheshwari <arp19690@users.noreply.github.com>
2025-08-17 16:33:13 -0700

    Bug fixes and more test coverage (#6)
    * updating the version number
    
    * udpating version
    
    * adding more tests
    
    * updating test coverage requirement
    
    * changes
    
    ---------
    
    Co-authored-by: Arpit Maheshwari <maheshwaria@microsoft.com>

--- jest.config.js
+++ jest.config.js
@@ -22,10 +22,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
     }
   },
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
